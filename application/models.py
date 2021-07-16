6c9e8b7ff7126fa79d31b34cf1b530276fb498c4
earl <theearlofgray@protonmail.com>
2021-07-16 12:11:38 +0000

    EG: Too long since last commit

--- application/models.py
+++ application/models.py
@@ -1,7 +1,16 @@
 from application import db
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField
 
 
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
-    desc = db.Column(db.String(50), default=" ")
+    desc = db.Column(db.String(50), default=" ")#
+    comp = db.Column(db.Boolean, default=False)
+
+class AddForm(FlaskForm):
+    name = StringField('Name')
+    desc = StringField('Description')
+    comp = BooleanField('Completed')
+    submit = SubmitField('Add Task')
--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,33 @@
+from werkzeug.utils import redirect
 from application import app, db
-from application.models import Tasks
+from application.models import Tasks, AddForm
+from flask import render_template
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.route('/add/<task_name>')
-def add(task_name):
-    new_task = Tasks(name=task_name)
-    db.session.add(new_task)
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = AddForm()
+    if form.validate_on_submit():
+        new_task = Tasks(name=form.name.data, desc=form.desc.data)
+        db.session.add(new_task)
+        db.session.commit()
+        items = Tasks.query.all()
+        return render_template('view_all.html', message="Task Added!", items=items)
+    else:
+        return render_template('add_task.html', form=form)
+
+@app.route('/view_all')
+def view_all():
+    items = Tasks.query.all()
+    return render_template('view_all.html', items=items)
+
+@app.route('/delete/<int:task>')
+def delete(task):
+    item = Tasks.query.get(task)
+    db.session.delete(item)
     db.session.commit()
-    return "Added new task to database"
+    return redirect('/view_all')
+
